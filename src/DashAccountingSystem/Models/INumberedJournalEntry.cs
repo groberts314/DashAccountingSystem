6f74d661f7712e4c9149bfb14e26ab5473f38216
Geoffrey Roberts <groberts314@yahoo.com>
2018-12-31 10:20:27 -0800

    Resolves #27 Refactor Journal Entry Links Partial

--- src/DashAccountingSystem/Models/INumberedJournalEntry.cs
+++ src/DashAccountingSystem/Models/INumberedJournalEntry.cs
@@ -1,7 +1,10 @@
-﻿namespace DashAccountingSystem.Models
+﻿using DashAccountingSystem.Data.Models;
+
+namespace DashAccountingSystem.Models
 {
     public interface INumberedJournalEntry
     {
         int EntryId { get; }
+        TransactionStatus Status { get; }
     }
 }
